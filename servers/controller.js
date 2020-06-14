c7916e50a33bb04ddc2fa4e45ed78fb9c7403d06
Jung-Min Kang <46243553+watxh@users.noreply.github.com>
2020-06-14 21:40:10 +0900

    firebase image server
    마더텅 떄려치우고 만든 것

--- servers/controller.js
+++ servers/controller.js
@@ -31,36 +31,36 @@ router.post('/b', function(req, res, next){
             res.status(200).send(req.body);
         });
 })
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads');  // 파일이 저장되는 경로입니다.
-    },
-    filename: function (req, file, cb) {
-        cb(null, moment().format('YYYYMMDDHHmmss') + "_" + file.originalname);  // 저장되는 파일명
-    }
-});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads');  // 파일이 저장되는 경로입니다.
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, moment().format('YYYYMMDDHHmmss') + "_" + file.originalname);  // 저장되는 파일명
+//     }
+// });
 
-const upload = multer({ storage: storage }).single("file");   // single : 하나의 파일업로드 할때
+// const upload = multer({ storage: storage }).single("file");   // single : 하나의 파일업로드 할때
 
-module.exports = upload;
+// module.exports = upload;
 
-router.post('/up', (req, res, next) => {
-    // FormData의 경우 req로 부터 데이터를 얻을수 없다.
-    // upload 핸들러(multer)를 통해서 데이터를 읽을 수 있다
+// router.post('/up', (req, res, next) => {
+//     // FormData의 경우 req로 부터 데이터를 얻을수 없다.
+//     // upload 핸들러(multer)를 통해서 데이터를 읽을 수 있다
     
-    upload(req, res, function(err) {
-      if (err instanceof multer.MulterError) {
-        return next(err);
-      } else if (err) {
-        return next(err);
-      }
-      console.log('원본파일명 : ' + req.file.originalname)
-      console.log('저장파일명 : ' + req.file.filename)
-      console.log('크기 : ' + req.file.size)
-      // console.log('경로 : ' + req.file.location) s3 업로드시 업로드 url을 가져옴
-      return res.json({success:1});
-    });
-  });
+//     upload(req, res, function(err) {
+//       if (err instanceof multer.MulterError) {
+//         return next(err);
+//       } else if (err) {
+//         return next(err);
+//       }
+//       console.log('원본파일명 : ' + req.file.originalname)
+//       console.log('저장파일명 : ' + req.file.filename)
+//       console.log('크기 : ' + req.file.size)
+//       // console.log('경로 : ' + req.file.location) s3 업로드시 업로드 url을 가져옴
+//       return res.json({success:1});
+//     });
+//   });
 
 router.get('/a', function(req, res) {
     User.find( {}, function(err, users) {
